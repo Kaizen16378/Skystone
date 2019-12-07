8c4c8e96282ea93dd30dfb385d6d4378ac228c8f
sai <sairangineeni1@gmail.com>
2019-12-06 19:28:25 -0500

    THis is the new code for the linear slide

--- skystone new921/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleopPOV_Linear.java
+++ skystone new921/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotTeleopPOV_Linear.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -36,68 +7,88 @@ import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 
-/**
- * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
- * All device access is managed through the HardwarePushbot class.
- * The code is structured as a LinearOpMode
- *
- * This particular OpMode executes a POV Game style Teleop for a PushBot
- * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
- * It raises and lowers the claw using the Gampad Y and A buttons respectively.
- * It also opens and closes the claws slowly using the left and right Bumper buttons.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 
-@TeleOp(name="Pushbot", group="Pushbot")
-// @Disabled
-public class PushbotTeleopPOV_Linear extends OpMode {
+@TeleOp(name="Teleop", group="Teleop")
+public class PushbotTeleopPOV_Linear extends OpMode
+{
     private DcMotor leftMotor = null;
-    private DcMotor linearSlideMotor = null;
     private DcMotor rightMotor = null;
+    private DcMotor tiltMotor = null;
+    private DcMotor extendMotor = null;
+    private Servo grabber = null;
+    private Servo tilt = null;
     private ElapsedTime runtime = new ElapsedTime();
-    private Servo pitch_clamp = null;
-    private Servo yaw_clamp = null;
-    private Servo roll_clamp = null;
-
-    public PushbotTeleopPOV_Linear() {
-    }
-
-    public void init() {
-        telemetry.addData("Status", "Initialized");
 
 
-        pitch_clamp = hardwareMap.get(Servo.class, "pitch_clamp");
-        yaw_clamp = hardwareMap.get(Servo.class, "yaw_clamp");
-        roll_clamp = hardwareMap.get(Servo.class, "roll_clamp");
+//***********************************************************************
+    public void init()
+    {
+        tilt = hardwareMap.get(Servo.class, "tilt");
+        grabber = hardwareMap.get(Servo.class, "grabber");
         leftMotor = hardwareMap.dcMotor.get("left_drive");
-        rightMotor = hardwareMap.dcMotor.get("right_drive");
-        linearSlideMotor = hardwareMap.dcMotor.get("slide_motor");
         leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor = hardwareMap.dcMotor.get("right_drive");
+        tiltMotor = hardwareMap.dcMotor.get("tilt_motor");
+        extendMotor = hardwareMap.dcMotor.get("extend_motor");
         telemetry.addData("Status", "Initialized");
     }
+//************************************************************************
 
-    public void loop() {
+
+    public void loop()
+    {
         double throttle = gamepad1.left_stick_y;
         double turn = gamepad1.right_stick_x;
-        double backslidespeed = gamepad2.left_trigger;
-        double frontslidespeed = gamepad2.right_trigger;
         double leftspeed = throttle - turn;
         double rightspeed = throttle + turn;
-        leftMotor.setPower(leftspeed);
-        linearSlideMotor.setPower(backslidespeed / 2.5);
-        rightMotor.setPower(rightspeed);
+        boolean tiltMotor_DirectionUp = gamepad2.dpad_up;
+        boolean tiltMotor_DirectionDown = gamepad2.dpad_down;
+        boolean tiltMotor_DirectionRight = gamepad2.dpad_right;
+        boolean tiltMotor_DirectionLeft = gamepad2.dpad_left;
+
+
+//********************DRIVE
+        leftMotor.setPower(leftspeed * 0.5);
+        rightMotor.setPower(rightspeed * 0.5);
         telemetry.addData("Status", "Run Time: " + runtime.toString());
+//*********************TILTING LINEAR SLIDE
+        if(tiltMotor_DirectionDown == true)
+        {
+            tiltMotor.setPower(0.5);
+        }
+        else
+            tiltMotor.setPower(0);
+
+        if(tiltMotor_DirectionUp == true)
+        {
+            tiltMotor.setPower(-0.5);
+        }
+        else
+            tiltMotor.setPower(0);
+
+
+//********************EXTENDING LINEAR SLIDE
+        if(tiltMotor_DirectionRight == true)
+        {
+            extendMotor.setPower(0.5);
+        }
+        if(tiltMotor_DirectionLeft == true)
+        {
+            extendMotor.setPower(-0.5);
+        }
+//********************GRABBING BLOCK-need to find right values
         if (gamepad2.a) {
-            pitch_clamp.setPosition(0);
+            grabber.setPosition(0);
         } else if (gamepad2.b) {
-            pitch_clamp.setPosition(0.5);
-
-
+            grabber.setPosition(0.5);
+        }
+//********************TILTING GRABBER-need to find right value
+        if (gamepad2.x) {
+            tilt.setPosition(0);
+        } else if (gamepad2.y) {
+            tilt.setPosition(0.5);
         }
     }
 }
